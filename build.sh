9595112b825c6a115b38041cde13795fbf8be7df
Salil Ponde <salil.ponde@gmail.com>
2019-03-06 18:51:14 +0000

    -

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ echo "Build script started"
 echo
 
 echo "Building Gateway Docker image"
-docker build -t nexus:8082/msp-gateway -f Dockerfile.Gateway
+docker build -t nexus:8082/msp-gateway -f Dockerfile.Gateway .
 docker push nexus:8082/msp-gateway
 echo "Finished building Gateway Docker image"
 
